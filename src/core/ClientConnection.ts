655008207fb9ba415af4912ea6f981d7c21469cc
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-03-22 22:18:39 +0000

    feat(core): use websockets to communicate with the client

--- src/core/ClientConnection.ts
+++ src/core/ClientConnection.ts
@@ -2,27 +2,62 @@ import { TrackInfo } from "./types"
 
 export default class ClientConnection {
     private static instance: ClientConnection
-    private url = "http://localhost:9104"
+    private static url = "ws://localhost:9104"
 
-    private constructor() {}
+    private socket: WebSocket
+    private token: string | null
+
+    private constructor() {
+        this.socket = new WebSocket(ClientConnection.url)
+        this.token = null
+
+        this.socket.onopen = () => this.onOpen()
+        this.socket.onclose = () => this.onClose()
+        this.socket.onmessage = (event) => this.onMessage(event)
+    }
 
     public static getInstance(): ClientConnection {
-        if (!ClientConnection.instance) {
+        if (!ClientConnection.instance)
             ClientConnection.instance = new ClientConnection()
-        }
 
         return ClientConnection.instance
     }
 
-    public async sendTrack(info: TrackInfo): Promise<boolean> {
-        const response = await fetch(`${this.url}/extension/update`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(info)
-        })
+    public sendTrack(info: TrackInfo) {
+        this.sendMessage("TRACK", info)
+    }
 
-        return response.ok
+    private onOpen() {
+        console.log("Connection opened with MediaMod client!")
+        this.sendMessage("HANDSHAKE", null)
+    }
+
+    // noinspection JSMethodCanBeStatic
+    private onClose() {
+        console.warn("Connection closed by the MediaMod client!")
+    }
+
+    private onMessage(event: MessageEvent) {
+        const message = JSON.parse(event.data)
+
+        switch (message.id) {
+            case "HANDSHAKE":
+                console.log("Received handshake from MediaMod client!")
+
+                this.token = message.data.token
+                break
+            case "HEARTBEAT":
+                break
+            default:
+                console.log(`Message received (${message.id}): ${message.data}`)
+                break
+        }
+    }
+
+    private sendMessage(id: string, data: Object | null) {
+        if (this.socket.readyState != WebSocket.OPEN) return
+        if (this.token == null && id != "HANDSHAKE") return
+
+        this.socket.send(JSON.stringify({ id: id, data: data, token: this.token }))
     }
 }
